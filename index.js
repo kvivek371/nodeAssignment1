b9ce945210ed538a607b7741d7abe120153f96b6
VIVEK KUMAR <51090910+kvivek371@users.noreply.github.com>
2022-07-01 19:38:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const http = require("http");
 // })
 
 http.createServer(function(req,res){
-    res.write("<h1>Hello</h1>");
+    res.write("<h1>Node : Node.js is a platform built on Chrome's JavaScript runtime for easily building fast and scalable network applications.</h1>");
     const jsonData = {name:"vivek",age:"26",course:"EA10"};
     res.write(JSON.stringify(jsonData));
     res.end();
